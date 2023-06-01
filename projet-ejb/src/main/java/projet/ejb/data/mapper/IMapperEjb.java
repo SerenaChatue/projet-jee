0fef2c0b95129d344eeb65c03552fffb9436461d
marcelle mazifa <mazifamm@3il.fr>
2023-06-01 14:17:44 +0200

    Service,dto,dao

--- projet-ejb/src/main/java/projet/ejb/data/mapper/IMapperEjb.java
+++ projet-ejb/src/main/java/projet/ejb/data/mapper/IMapperEjb.java
@@ -3,7 +3,9 @@ package projet.ejb.data.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import projet.commun.dto.DtoCategorie;
 import projet.commun.dto.DtoCompte;
+import projet.ejb.data.Categorie;
 import projet.ejb.data.Compte;
 
  
@@ -19,4 +21,9 @@ public interface IMapperEjb {
 	
 	DtoCompte map( Compte source );
 	
+	// Categorie
+	
+		Categorie map( DtoCategorie source );
+		
+		DtoCategorie map( Categorie source );
 }
